4ae27c7978438d140a84c5eac36af5289cf4be66
Lifu Huang <lifu.hlf@gmail.com>
2017-09-15 01:18:04 -0700

    Finish most of svm.

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -35,13 +35,17 @@ def svm_loss_naive(W, X, y, reg):
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
         loss += margin
+        dW[:, j] += X[i]
+        dW[:, y[i]] += -X[i]
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
+  dW /= num_train
 
   # Add regularization to the loss.
   loss += reg * np.sum(W * W)
+  dW += reg * 2 * W
 
   #############################################################################
   # TODO:                                                                     #
@@ -70,7 +74,14 @@ def svm_loss_vectorized(W, X, y, reg):
   # Implement a vectorized version of the structured SVM loss, storing the    #
   # result in loss.                                                           #
   #############################################################################
-  pass
+
+  scores = X @ W
+  num_train, num_classes = scores.shape
+
+  margins = scores - scores[range(num_train), y][:, np.newaxis] + 1
+  margins[range(num_train), y] = 0
+  mask = margins > 0
+  loss = np.sum(margins[mask]) / num_train + reg * np.sum(W * W)
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
@@ -85,7 +96,11 @@ def svm_loss_vectorized(W, X, y, reg):
   # to reuse some of the intermediate values that you used to compute the     #
   # loss.                                                                     #
   #############################################################################
-  pass
+
+  coef_mat = mask.copy().astype(int)
+  coef_mat[range(num_train), y] -= np.sum(mask, axis=1)
+
+  dW = X.T @ coef_mat / num_train + 2 * reg * W
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
